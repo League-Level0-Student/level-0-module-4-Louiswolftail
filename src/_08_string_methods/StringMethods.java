2e85737a5f1dc6649b1aeb3445675690a9b39314
368325 <368325@0338S-S-R0B6ZHR.sdcs.local>
2020-01-14 14:22:31 -0800

    2020

--- src/_08_string_methods/StringMethods.java
+++ src/_08_string_methods/StringMethods.java
@@ -2,23 +2,22 @@ package _08_string_methods;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
-
 public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String looped =  "UpUpdown";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(looped.charAt(7));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		System.out.println(looped.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
-		
+		for(int i=0; i<looped.length();i++) {
+			System.out.println(looped.charAt(i));
+		}
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
