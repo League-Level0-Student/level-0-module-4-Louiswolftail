7d039bfe3d2394a2c5fe58fd0c870c9cbd93b25d
368325 <368325@0338S-S-R0B6ZHR.sdcs.local>
2020-01-21 14:12:44 -0800

    2020 pt 2

--- src/_09_pi_aloud/PiAloud.java
+++ src/_09_pi_aloud/PiAloud.java
@@ -6,23 +6,28 @@ package _09_pi_aloud;
 import java.io.IOException;
 import java.util.Scanner;
 
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
 public class PiAloud {
-
+	
 	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+ String pi = "3.1415926535";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
+ System.out.println(pi.charAt(11));
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
+for (int i = 0; i < pi.charAt(11); i++) {
+	System.out.println(i);
+}
 	// 5. Use the speak() method to speak all the digits of Pi.
-
+ speak(pi.charAt(11));
 	// [ADVANCED]
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
+ 
+ }
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
@@ -46,5 +51,6 @@ public class PiAloud {
 	}
 
 }
+
 
 
